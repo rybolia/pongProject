e1bd3725defc5fba870344ef28ac64d0eca42649
Derek Donev <derekdonev@gmail.com>
2021-03-20 15:57:43 -0400

    added paddles and ball to updateCanvas function
    SetInterval calls updateCanvas every second

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 let c = document.getElementById("myCanvas");
 let ctx = c.getContext("2d");
 
-// Left Paddle
-ctx.fillStyle = "white";
-ctx.fillRect(10, 265, 10, 75);
-// Right Paddle
-ctx.fillStyle = "white";
-ctx.fillRect(780, 265, 10, 75);
+function updateCanvas() {
+    // draw the canvas every 30 seconds
+    // Left Paddle
+    ctx.fillStyle = "white";
+    ctx.fillRect(10, 265, 10, 75);
+    // Right Paddle
+    ctx.fillStyle = "white";
+    ctx.fillRect(780, 265, 10, 75);
 
-// Pong ball
-ctx.fillStyle = "white";
-ctx.beginPath();
-ctx.arc(400, 300, 7, 0, 2 * Math.PI);
-ctx.closePath();
-ctx.fill();
+    // Pong ball
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(400, 300, 7, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.fill();
 
-// Middle Line
-// setLineDash, separation size of space between lines
-ctx.strokeStyle = "white";
-ctx.beginPath();
-ctx.setLineDash([10]);
-ctx.moveTo(400, 0);
-ctx.lineTo(400, 600);
-ctx.stroke();
-ctx.closePath();
+    // Middle Line
+    // setLineDash, separation size of space between lines
+    ctx.strokeStyle = "white";
+    ctx.beginPath();
+    ctx.setLineDash([10]);
+    ctx.moveTo(400, 0);
+    ctx.lineTo(400, 600);
+    ctx.stroke();
+    ctx.closePath();
+}
+setInterval(updateCanvas, 1000);
 
 // Player1's x and y coordinates
 let playerOneX = 0;
